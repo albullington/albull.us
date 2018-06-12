209313df53ea7189dd570a1495e988da618189a4
Amanda <albullington@gmail.com>
2018-06-11 20:57:51 -0700

    added react router

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Header from './header';
 import Footer from './footer';
+import Engineer from './engineer';
+import Communicator from './communicator';
+import Athlete from './athlete';
 
 const App = () => (
+  <Router>
   <div>
-    <Header />
-    <Footer />
+  <Header />
+  <Route exact path="/" component={Home} />
+  <Route path="/engineer" component={Engineer} />
+  <Route path="/communicator" component={Communicator} />
+  <Route path="/athlete" component={Athlete} />
+  <Footer />
   </div>
+  </Router>
 );
 
 export default App;
