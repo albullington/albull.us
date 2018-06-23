e59fbbe35029e80f96eaef270362865d0e82da9f
Amanda <albullington@gmail.com>
2018-06-23 16:11:51 -0700

    updated git ignore for heroku

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.get('/', (req, res) => res.send('Hello, client'));
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log(`Listening on http://localhost:${port}`));
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
