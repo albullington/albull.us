2b209cfe6a529beee8d6c18d19c3a4920d610b3b
Amanda <albullington@gmail.com>
2018-06-23 14:09:13 -0700

    added project to engineering

--- client/components/engineer.jsx
+++ client/components/engineer.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { BodyText } from '../style';
+import { BodyTextCenter, BodyLink } from '../style';
+
+import twitter from '../img/engineer/twitter-analytics.png';
+
+const imgStyle = {
+  display: 'block',
+  width: '60%',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+};
 
 const Engineer = () => (
   <div>
-    <BodyText>I love problem solving. As an engineer, I build web apps in React, JavaScript, and Node.js. Before moving into engineering, I built new marketing programs and processes from the ground up at San Francisco fintech startups NerdWallet and Ripple.</BodyText>
-    <BodyText>Here are some examples of my projects:</BodyText> 
+    <BodyTextCenter>I love problem solving. As an engineer, I build web apps in React, JavaScript, and Node.js. Before moving into engineering, I built new marketing programs and processes from the ground up at San Francisco fintech startups NerdWallet and Ripple.</BodyTextCenter>
+    <BodyTextCenter>Here are some examples of my projects:</BodyTextCenter> 
+    <BodyLink href="https://github.com/albullington/twitter-sentiments">
+    <img src={twitter} style={imgStyle} />
+    </BodyLink>
   </div>
 )
 
