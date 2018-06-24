91de99041b091a84db952e8d88309ebc28626c84
Amanda <albullington@gmail.com>
2018-06-23 18:08:28 -0700

    added google analytics

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from './header';
 import Footer from './footer';
