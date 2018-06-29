a3d4dac6e7fe8adf7b722d790ca2d5dc4ce6ac4a
Amanda <albullington@gmail.com>
2018-06-28 18:28:01 -0700

    added new project to engineering tab

--- client/components/engineer.jsx
+++ client/components/engineer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BodyText, BodyLink } from '../style';
 
+import crypto from '..//img/engineer/crypto-price-tracker.png'
 import twitter from '../img/engineer/twitter-analytics.png';
 import historical from '../img/engineer/historical-tracker.png';
 
@@ -15,6 +16,9 @@ const Engineer = () => (
   <div>
     <BodyText>Problem solving is fun! As an engineer, I build web apps -- typically in React, JavaScript, and Node.js.</BodyText>
     <BodyText>Here are some examples of my projects, which you can also find on my <BodyLink href="https://github.com/albullington/">GitHub:</BodyLink></BodyText> 
+    <BodyLink href="https://github.com/albullington/ethereum-price-history">
+    <img src={crypto} style={imgStyle} />
+    </BodyLink>
     <BodyLink href="https://github.com/albullington/twitter-sentiments">
     <img src={twitter} style={imgStyle} />
     </BodyLink>
